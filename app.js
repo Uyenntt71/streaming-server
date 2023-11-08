c3739488065573377fe9a44ce5952c84e07d6a85
Uyen <uyenntt71@viettel.com.vn>
2023-11-08 18:18:58 +0700

    update mmpegdash command, remember create folder before run cmd

--- app.js
+++ app.js
@@ -15,10 +15,14 @@ app.use("/", userRouter);
 
 app.post("/encode-video", async (req, res) => {
   const publicFolderPath = path.join(__dirname, "public");
-  const inputFilePath = path.join(publicFolderPath, "video/totoro.mp4");
-  const outputHLSPath = path.join(publicFolderPath, "hls/totoro");
-  const command = `ffmpeg -i ${inputFilePath} -b:v 1M -g 60 -hls_time 2 -hls_list_size 0 -hls_segment_size 500000 ${outputHLSPath}`;
-  console.log(command);
+  const inputFilePath = path.join(
+    publicFolderPath,
+    "video/Sticking-Seafarer_by-Jeremy-Ross.mp4"
+  );
+  const outputHLSPath = path.join(
+    publicFolderPath,
+    "hls/Sticking-Seafarer_by-Jeremy-Ross"
+  );
   const ffmpegCommand = `ffmpeg -i ${inputFilePath} -map 0:v:0 -map 0:a:0 -map 0:v:0 -map 0:a:0 \
   -c:v h264 -profile:v main -crf 20 -sc_threshold 0 -g 48 -keyint_min 48 -c:a aac -ar 48000 \
   -filter:v:0 scale=w=640:h=360:force_original_aspect_ratio=decrease -maxrate:v:0 350k -bufsize:v:0 1200k -b:a:0 96k \
@@ -34,21 +38,25 @@ app.post("/encode-video", async (req, res) => {
       return;
     }
 
-    console.log("FFmpeg command executed successfully.");
     res.json({ message: "FFmpeg command executed successfully." });
   });
 });
 
 app.post("/encode-video-dash", async (req, res) => {
   const publicFolderPath = path.join(__dirname, "public");
-  const inputFilePath = path.join(publicFolderPath, "video/totoro.mp4");
-  const outputDASHPath = path.join(publicFolderPath, "dash/totoro/output.mpd");
-  const ffmpegCommand = `ffmpeg -i ${inputFilePath} \
-  -map 0:v:0 -map 0:v:0 -map 0:a\\?:0  \
-  -b:v:0 350k  -c:v:0 libx264 -filter:v:0 "scale=320:-1"  \
-  -b:v:2 3000k -c:v:1 libx264 -filter:v:2 "scale=1280:-1" \
-  -use_timeline 0 -use_template 1 -adaptation_sets "id=0,streams=v  id=1,streams=a" \
-      -f dash ${outputDASHPath}`;
+  const inputFilePath = path.join(publicFolderPath, "video/totoro2.mp4");
+  const outputDASHPath = path.join(publicFolderPath, "dash/totoro2");
+  // const ffmpegCommand = `ffmpeg -i ${inputFilePath} \
+  // -b:v:0 350k  -c:v:0 libx264 -filter:v:0 "scale=320:-1"  \
+  // -b:v:2 3000k -c:v:1 libx264 -filter:v:2 "scale=1280:-1" \
+  // -use_timeline 0 -use_template 1 -adaptation_sets "id=0,streams=v  id=1,streams=a" \
+  //     -f dash ${outputDASHPath}/output.mpd`;
+  const ffmpegCommand = `ffmpeg -re -i ${inputFilePath} -map 0 -map 0 -c:a aac -c:v libx264 \
+  -b:v:0 800k -b:v:1 300k -s:v:1 320x170 -profile:v:1 baseline \
+  -profile:v:0 main -bf 1 -keyint_min 120 -g 120 -sc_threshold 0 \
+  -b_strategy 0 -ar:a:1 22050 -use_timeline 1 -use_template 1 \
+  -window_size 5 -adaptation_sets "id=0,streams=v id=1,streams=a" \
+  -f dash ${outputDASHPath}/output.mpd`;
   exec(ffmpegCommand, (error, stdout, stderr) => {
     if (error) {
       console.error("Error:", error);
